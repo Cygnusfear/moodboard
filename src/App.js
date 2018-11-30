e41eca6c35995bb611b374f41c8775fd93d71452
Alexander Mangel <cygnusfear@gmail.com>
2018-11-30 20:24:02 +0100

    drag and drop

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import StackGrid from "react-stack-grid";
 import ImageZoom from "react-medium-image-zoom";
+import DropZone from "react-magic-dropzone";
 
 const shuffleArray = arr =>
   arr
@@ -22,25 +23,84 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      images: images
+      images: images,
+      dragging: false
     };
+    this.dropArea = React.createRef();
   }
+
+  onDrop = (accepted, rejected, links) => {
+    console.log(accepted); // Have fun
+  };
 
   componentDidMount() {
-    // setInterval(() => {
-    //   console.log("wut");
-    //   let i = shuffleArray(images);
-    //   this.setState({ images: i });
-    //   this.grid.updateLayout();
-    // }, 20000);
+    console.log(this.dropArea);
+    this.dropArea.current.addEventListener("dragover", this.fileDrag, false);
+    this.dropArea.current.addEventListener("dragenter", this.fileDrag, false);
+
+    this.dropArea.current.addEventListener("drop", this.fileDrop, false);
+    this.dropArea.current.addEventListener("dragleave", this.leaveDrag, false);
   }
 
+  fileDrag = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    // console.log("drag", e);
+    if (!this.state.dragging) this.setState({ dragging: true });
+  };
+
+  leaveDrag = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    // this.setState({ dragging: false });
+    if (e.dataTransfer.files.length > 0)
+      console.log("drop", e.dataTransfer.files);
+  };
+
+  fileDrop = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.dataTransfer.files.length > 0)
+      console.log("drop", e.dataTransfer.files);
+    let images = [];
+    [...e.dataTransfer.files].forEach(img => {
+      images.push(URL.createObjectURL(img));
+    });
+    this.setState({
+      dragging: false,
+      images: [...this.state.images, ...images]
+    });
+  };
+
+  uploadFile = file => {
+    let url = "YOUR URL HERE";
+    let formData = new FormData();
+
+    formData.append("file", file);
+
+    fetch(url, {
+      method: "POST",
+      body: formData
+    })
+      .then(() => {
+        /* Done. Inform the user */
+      })
+      .catch(() => {
+        /* Error. Inform the user */
+      });
+  };
+
   render() {
-    console.log(images);
+    const { dragging } = this.state;
     return (
       <div className="App">
-        <header className="App-header" />
-        <div className="Gallery">
+        <div
+          className="Gallery"
+          ref={this.dropArea}
+          style={{
+            opacity: dragging ? 0.1 : 1
+          }}
+        >
           <StackGrid
             gridRef={grid => (this.grid = grid)}
             columnWidth={"30%"}
@@ -51,17 +111,15 @@ class App extends Component {
             monitorImagesLoaded={true}
           >
             {this.state.images.map((item, i) => (
-              <div key={i}>
+              <div
+                key={i}
+                style={{
+                  pointerEvents: dragging ? "none" : "auto"
+                }}
+              >
                 <ImageZoom
-                  image={{
-                    src: item,
-                    className: "img",
-                    title: item
-                  }}
-                  zoomImage={{
-                    alt: "Golden Gate Bridge",
-                    src: item
-                  }}
+                  image={{ src: item, className: "img", title: item }}
+                  zoomImage={{ alt: "Golden Gate Bridge", src: item }}
                 />
               </div>
             ))}
