68d11d206ea787248126c212ae5a37782ce420fb
Alexander Mangel <cygnusfear@gmail.com>
2020-01-25 11:39:02 +0100

    zeit now

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import "./App.css";
-import StackGrid from "react-stack-grid";
-import ImageZoom from "react-medium-image-zoom";
+import React, { Component } from 'react';
+import './App.css';
+import StackGrid from 'react-stack-grid';
+import ImageZoom from 'react-medium-image-zoom';
 
 // const shuffleArray = arr =>
 //   arr
@@ -17,7 +17,7 @@ const gutter = 30;
 const images = [];
 
 const imported = importAll(
-  require.context("./Images/", false, /\.(png|jpe?g|svg)$/)
+  require.context('./Images/', true, /\.(png|jpe?g|svg)$/),
 );
 
 console.log(imported);
@@ -28,7 +28,7 @@ class App extends Component {
     this.state = {
       images: images,
       dragging: false,
-      selected: null
+      selected: null,
     };
     this.dropArea = React.createRef();
   }
@@ -39,16 +39,18 @@ class App extends Component {
 
   componentDidMount() {
     if (imported.length > 0) {
-      this.setState({images: imported});
+      this.setState({ images: imported });
     }
     console.log(this.dropArea);
-    this.dropArea.current.addEventListener("dragover", this.fileDrag, false);
-    this.dropArea.current.addEventListener("dragenter", this.fileDrag, false);
+    this.dropArea.current.addEventListener('dragover', this.fileDrag, false);
+    this.dropArea.current.addEventListener('dragenter', this.fileDrag, false);
 
-    this.dropArea.current.addEventListener("drop", this.fileDrop, false);
-    this.dropArea.current.addEventListener("dragleave", this.leaveDrag, false);
-    window.addEventListener("keydown", this.keyPress);
+    this.dropArea.current.addEventListener('drop', this.fileDrop, false);
+    this.dropArea.current.addEventListener('dragleave', this.leaveDrag, false);
+    window.addEventListener('keydown', this.keyPress);
   }
+
+  fileAdd = url => {};
 
   fileDrag = e => {
     e.preventDefault();
@@ -66,7 +68,7 @@ class App extends Component {
     e.preventDefault();
     e.stopPropagation();
     if (e.dataTransfer.files.length > 0)
-      console.log("drop", e.dataTransfer.files);
+      console.log('drop', e.dataTransfer.files);
     let images = [];
     [...e.dataTransfer.files].forEach(img => {
       console.log(img);
@@ -74,7 +76,7 @@ class App extends Component {
     });
     this.setState({
       dragging: false,
-      images: [...this.state.images, ...images]
+      images: [...this.state.images, ...images],
     });
   };
 
@@ -87,7 +89,7 @@ class App extends Component {
   };
 
   keyPress = e => {
-    if (this.state.selected && e.code === "Backspace") {
+    if (this.state.selected && e.code === 'Backspace') {
       let images = [...this.state.images];
       images.splice(images.indexOf(this.state.selected), 1);
       this.setState({ images: images, selected: null });
@@ -104,10 +106,10 @@ class App extends Component {
           style={{ opacity: dragging ? 0.1 : 1 }}
         >
           {images.length === 0 &&
-            "Drop images here, make moodboard, wow much! ❤️"}
+            'Drop images here, make moodboard, wow much! ❤️'}
           <StackGrid
             gridRef={grid => (this.grid = grid)}
-            columnWidth={"30%"}
+            columnWidth={'30%'}
             gutterWidth={gutter}
             gutterHeight={gutter - 5}
             duration={0}
@@ -118,14 +120,14 @@ class App extends Component {
               <div
                 key={i}
                 style={{
-                  pointerEvents: dragging ? "none" : "auto"
+                  pointerEvents: dragging ? 'none' : 'auto',
                 }}
                 onMouseEnter={() => this.hoverOn(item)}
                 onMouseLeave={() => this.hoverOff(item)}
               >
                 <ImageZoom
-                  image={{ src: item, className: "img", title: item }}
-                  zoomImage={{ className: "zoomed", src: item }}
+                  image={{ src: item, className: 'img', title: item }}
+                  zoomImage={{ className: 'zoomed', src: item }}
                 />
               </div>
             ))}
